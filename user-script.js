441cde90b45672967d76d7213836cdd9f970080c
Charlie Sharpsteen <chuck@puppet.com>
2023-09-23 09:41:57 -0500

    Re-factor into a basic module
    This commit does not make any functional changes, but splits the
    user script up so that it is no longer one big monolithic function.

--- user-script.js
+++ user-script.js
@@ -4,40 +4,50 @@
 // @require  https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.15/codemirror.min.js
 // @require  https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.15/mode/javascript/javascript.min.js
 // @grant    none
+// @include  https://*.delivery.puppetlabs.net/*
 // @run-at   document-start
 // ==/UserScript==
 
-addEventListener("DOMContentLoaded", function(){
-  var link = document.createElement("link");
+(function() {
+  function initCodeMirrorCSS() {
+    let cm_css = document.createElement("link");
 
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.href = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.15/codemirror.min.css";
+    cm_css.type = "text/css";
+    cm_css.rel = "stylesheet";
+    cm_css.href = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.15/codemirror.min.css";
 
-  document.head.appendChild(link);
+    document.head.appendChild(cm_css);
+  }
 
-  let observer = new MutationObserver(mutations => {
+  function initTextareaObservers() {
+    let observer = new MutationObserver(mutations => {
 
-    for(let mutation of mutations) {
-      // examine new nodes, is there anything to highlight?
+      for(let mutation of mutations) {
+        // examine new nodes, is there anything to highlight?
 
-      for(let node of mutation.addedNodes) {
-        // we track only elements, skip other nodes (e.g. text nodes)
-        if (!(node instanceof HTMLElement)) continue;
+        for(let node of mutation.addedNodes) {
+          // we track only elements, skip other nodes (e.g. text nodes)
+          if (!(node instanceof HTMLElement)) continue;
 
-        // check the inserted element for being a code snippet
-        if (node.matches('textarea[class*="class-param-add-value"]')) {
-          node.value = "hello, world"
+          // check the inserted element for being a code snippet
+          if (node.matches('textarea[class*="class-param-add-value"]')) {
+            node.value = "hello, world"
 
-          CodeMirror.fromTextArea(node, {
-            mode: "application/json",
-            lineNumbers: true
-          });
+            CodeMirror.fromTextArea(node, {
+              mode: "application/json",
+              lineNumbers: true
+            });
+          }
         }
       }
-    }
 
+    });
+
+    observer.observe(document.body, {childList: true, subtree: true});
+  }
+
+  addEventListener("DOMContentLoaded", function(){
+    initCodeMirrorCSS();
+    initTextareaObservers();
   });
-
-  observer.observe(document.body, {childList: true, subtree: true});
-});
+})();
